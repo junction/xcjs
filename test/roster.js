eecb50da3eca9d5d14cfe4e907a3149a15d3bed8
Tim Evans <tim.evans@junctionnetworks.com>
2010-09-03 12:03:16 -0400

    Passing tests for roster update on an entity.

--- test/roster.js
+++ test/roster.js
@@ -6,11 +6,20 @@ XC.Test.Roster = new YAHOO.tool.TestCase({
     this.conn = XC.Test.MockConnection.extend().init();
     this.xc = XC.Connection.extend({connection: this.conn});
     this.xc.initConnection();
+
+    this.marvin = XC.Entity.extend({
+      name: "Marvin",
+      jid: "marvin@heart-of-gold.com",
+      connection: this.xc
+    });
   },
 
   tearDown: function () {
     delete this.conn;
     delete this.xc;
+  },
+
+  testRequest: function () {
   },
 
   testAdd: function () {
@@ -20,9 +29,29 @@ XC.Test.Roster = new YAHOO.tool.TestCase({
   },
 
   testUpdate: function () {
-  },
+    var Assert = YAHOO.util.Assert;
 
-  testRequest: function () {
+    this.conn.addResponse(XC.Test.Packet.extendWithXML(
+      '<iq to="marvin@heart-of-gold.com" \
+           type="result" \
+           id="test"\>'
+    ));
+    
+    var fail = false, win = false, that = this;
+    this.marvin.rosterUpdate({
+      onSuccess: function (entity) {
+        win = true;
+        Assert.isObject(entity);
+        Assert.areEqual(entity, that.marvin);
+      },
+
+      onError: function () {
+        fail = true;
+      }
+    });
+
+    Assert.isTrue(win, "Was not successful in doing a roster update.");
+    Assert.isFalse(fail, "Roster update threw an error.");  
   }
 
 });
