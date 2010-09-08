9d0ec170abc6fd68615ec804cd22c84aeff20461
Erick Johnson <erick@junctionnetworks.com>
2010-09-08 14:11:39 -0400

    did stuff

---
+++ test/entity.js
@@ -0,0 +1,6 @@
+/*globals YAHOO */
+XC.Test.Base = new YAHOO.tool.TestCase({
+  name: 'XC.Entity Tests'
+});
+
+YAHOO.tool.TestRunner.add(XC.Test.Base);
---
+++ test/mixins/chat-recipient.js
@@ -0,0 +1,31 @@
+/*globals YAHOO */
+XC.Test.ChatRecipient = new YAHOO.tool.TestCase({
+  name: 'XC.Mixin.ChatRecipient Tests',
+
+  setUp: function () {
+    this.conn = XC.Test.MockConnection.extend().init();
+    this.xc = XC.Connection.extend({connectionAdapter: this.conn});
+    this.xc.initConnection();
+  },
+
+  tearDown: function () {
+    delete this.conn;
+    delete this.xc;
+  },
+
+  testMixin: function () {
+    var Assert = YAHOO.util.Assert;
+
+    Assert.isObject(XC.Mixins.ChatRecipient, '');
+
+    var tmp = XC.Object.extend(XC.Mixins.ChatRecipient);
+
+    Assert.isFunction(tmp.sendChat, 'sendChat is not a function');
+  },
+
+  testSendChat: function() {
+
+  }
+});
+
+YAHOO.tool.TestRunner.add(XC.Test.Base);
