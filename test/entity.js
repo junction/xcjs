8ab286dda5114e12db87233025cd2b34a4998c47
Erick Johnson <erick@junctionnetworks.com>
2010-09-09 14:24:52 -0400

    added a test for the XC.Entity object

--- test/entity.js
+++ test/entity.js
@@ -1,6 +1,11 @@
 /*globals YAHOO */
 XC.Test.Base = new YAHOO.tool.TestCase({
-  name: 'XC.Entity Tests'
+  name: 'XC.Entity Tests',
+
+  testIsObject: function() {
+    var Assert = YAHOO.util.Assert;
+    Assert.isObject(XC.Entity);
+  }
 });
 
 YAHOO.tool.TestRunner.add(XC.Test.Base);
